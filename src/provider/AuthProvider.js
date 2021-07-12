c0a1f6c2ac729bb3838d222479eb19ab381c1b5c
Apriadi <dev.apriadi@gmail.com>
2021-07-12 17:25:13 +0700

    mengubah method auth

--- src/provider/AuthProvider.js
+++ src/provider/AuthProvider.js
@@ -3,6 +3,7 @@ import auth from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import SplashScreen from 'react-native-splash-screen'
 import { BASE_URL } from "@env"
+import axios from 'axios'
 
 export const AuthContext = createContext();
 
@@ -10,13 +11,17 @@ export const AuthProvider = ({ children }) => {
     const baseUrl = BASE_URL
     const [initializing, setInitializing] = useState(true);
     const [user, setUser] = useState(null);
+    const [loadingAuth, setLoadingAuth] = useState(true)
 
     const onAuthStateChanged = async (user) => {
         await setUser(user);
         SplashScreen.hide();
         if (initializing){
           setInitializing(false);
-          SplashScreen.hide();
+          
+          if(user){
+            getAuthentication(user)
+          }
         }
     }
 
@@ -39,13 +44,34 @@ export const AuthProvider = ({ children }) => {
         });
     };
 
+    const getAuthentication = async(user) => {
+      await axios({
+            method: 'POST',
+            url: `${baseUrl}/api/user`,
+            data: {
+                  name: user._user.displayName ??  user._user.phoneNumber,
+                  uid:  user._user.uid, 
+                  profilePicture:  user._user.photoURL,
+                  tokenNotif: null
+            },
+            headers:{
+                  Accept: 'Aplication/json'
+            }
+      }).then(res => {
+        setUser({...user, idForUser: res.data.data.id})
+      }).catch(err => {
+          console.log(err)
+      })
+      setLoadingAuth(false)
+    }
+
       useEffect(() => {
         SplashScreen.show();
         const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
         return subscriber;
       }, []);
 
-    const AuthState = { user, setUser, initializing, signOut, baseUrl };
+    const AuthState = { user, setUser, initializing, signOut, baseUrl, loadingAuth };
 
     return(
         <AuthContext.Provider value={AuthState}>
